7f3d0be24020da37e2fb99e7635e0d41aaa68f63
nicole03 <ntidwell03@gmail.com>
2018-07-24 18:46:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,10 @@
 number = 6 
-
 if number%3 == 0 
   puts "fizz"
-end 
-
 number = 15
-if number%5 == 0 
+elsif number%5 == 0 
   puts "buzz"
 number = 30 
-if number%15 == 0 
+elsif number%15 == 0 
   puts "fizzbuzz"
 end 
